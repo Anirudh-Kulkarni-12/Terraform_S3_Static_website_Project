2995a63b028a04d5800f5a1b5bdc62caca91696b
Anirudh <aani.spark@gmail.com>
2024-08-01 17:14:52 +0530

    s3 bucket owner rule changed

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_ownership_controls" "mybucket_owner" {
     bucket = aws_s3_bucket.mybucket.id
 
     rule {
-      object_ownership = aws_s3_bucket.mybucket
+      object_ownership = "BucketOwnerPreferred"
     }
 }
 
@@ -18,4 +18,14 @@ resource "aws_s3_bucket_public_access_block" "mybucket_public_access" {
     block_public_policy = false
     ignore_public_acls = false
     restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "mybucket_acl" {
+    depends_on = [ 
+        aws_s3_bucket_ownership_controls.mybucket_owner,
+        aws_s3_bucket_public_access_block.mybucket_public_access,
+     ]  
+
+     bucket = aws_s3_bucket.mybucket.id
+     acl = "public-read"
 }
